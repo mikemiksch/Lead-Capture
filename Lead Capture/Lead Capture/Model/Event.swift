3807267d88b8e3262364ac2608149c85f31575f3
mikemiksch <mikemiksch@gmail.com>
2017-11-13 15:43:46 -0800

    Worked on some of the variables for the leads and event objects

---
+++ Lead Capture/Lead Capture/Model/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  Lead Capture
+//
+//  Created by Mike Miksch on 11/10/17.
+//  Copyright © 2017 Mike MIksch. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    var id : UUID
+}
---
+++ Lead Capture/Lead Capture/Model/Lead.swift
@@ -0,0 +1,18 @@
+//
+//  Lead.swift
+//  Lead Capture
+//
+//  Created by Mike Miksch on 11/10/17.
+//  Copyright © 2017 Mike MIksch. All rights reserved.
+//
+
+import Foundation
+
+class Lead {
+    var eventID : UUID
+    var name : String
+    var email : String
+    var date : Date
+    var phoneNum : String
+    var comments: String
+}
