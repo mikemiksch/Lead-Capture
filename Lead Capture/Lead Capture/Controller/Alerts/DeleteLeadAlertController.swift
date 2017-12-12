ae46f1d7bdfc3b26941b22ef62073fcb10023529
mikemiksch <mikemiksch@gmail.com>
2017-12-12 12:45:41 -0800

    Styled DeleteLeadAlert in the same manner as the other alerts.

--- Lead Capture/Lead Capture/Controller/Alerts/DeleteLeadAlertController.swift
+++ Lead Capture/Lead Capture/Controller/Alerts/DeleteLeadAlertController.swift
@@ -14,6 +14,9 @@ class DeleteLeadAlertController: UIViewController {
     var index : Int!
     var delegate : DeleteLeadDelegate?
     
+    @IBOutlet weak var viewBox: UIView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     @IBAction func cancelButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -25,9 +28,28 @@ class DeleteLeadAlertController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        applyFormatting()
+    }
+    
+    func applyFormatting() {
+        viewBox.layer.cornerRadius = 10
+        viewBox.layer.borderColor = Settings.shared.accentColor.cgColor
+        viewBox.layer.borderWidth = 1
+        viewBox.layer.shadowColor = UIColor.black.cgColor
+        viewBox.layer.shadowOpacity = 0.5
+        viewBox.layer.shadowOffset = CGSize.zero
+        viewBox.layer.shadowRadius = 5
+        viewBox.layer.shadowPath = UIBezierPath(rect: viewBox.bounds).cgPath
+        cancelButton.layer.cornerRadius = 10
+        deleteButton.layer.cornerRadius = 10
+        cancelButton.backgroundColor = Settings.shared.accentColor
+        cancelButton.setTitleColor(Settings.shared.backgroundColor, for: .normal)
+        deleteButton.backgroundColor = Settings.shared.backgroundColor
+        deleteButton.setTitleColor(Settings.shared.accentColor, for: .normal)
     }
 
 }
+
 
 protocol DeleteLeadDelegate {
     func deleteLead(index: Int, lead: Lead)
